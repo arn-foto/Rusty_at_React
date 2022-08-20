df6b9785785244e4e1f10b68ade61846975cfbac
Anthony-Navarro <foto.arn@gmail.com>
2022-08-20 14:55:15 -0700

    Set up - Header,Body,Footer

---
+++ src/Body/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { AppBody } from "../Footer/styles";
+
+function Body() {
+  return <AppBody>This is the body</AppBody>;
+}
+
+export default Body;
---
+++ src/Footer/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AppBody = styled.div`
+  display: flex;
+  background-color: grey;
+  background-attachment: fixed;
+  background-size: cover;
+  padding-left: 20px;
+`;
+
+export { AppBody };
